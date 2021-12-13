dbf145b1391bc78cea631794ec1660ebe3d09b30
James Burns <jrburns96@aol.com>
2021-12-13 16:00:12 -0500

    commit tree 34442751a5815edfa0fe2c4e3ef55fea17b5710e

---
+++ app.rb
@@ -0,0 +1,5 @@
+# app.rb
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+p "Pass this test, please."
+puts "Hello World!"
+pp [1,2,3]
